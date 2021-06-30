27c0faab6a9b4f788d765938888020d7939081d6
Christian <christian@christians-air.home>
2021-06-30 21:33:55 +0100

    layout & styles added

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const imgContainer = document.querySelectorAll('image-container')
+
+const btnLeft = document.getElementById('left-btn')
+const btnRight = document.getElementById('right-btn')
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
